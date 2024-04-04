d00d509b6a7486a0302500336eaf22a77fc4dc23
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-04-04 17:36:38 +0300

    Update sqlite_models.dart

--- lib/data/models/sqlite_models.dart
+++ lib/data/models/sqlite_models.dart
@@ -7,68 +7,27 @@ import 'package:drift/wasm.dart';
 import 'package:drift/drift.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:spektr/data/models/sqlite_tables.dart';
 import 'package:sqlite3/sqlite3.dart';
 
 part 'sqlite_models.g.dart';
 
-class RentVehicle extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text()();
-  IntColumn get price => integer()();
-  TextColumn get description => text()();
-  BlobColumn get image => blob()();
-}
-
-class Work extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text()();
-  IntColumn get price => integer()();
-  TextColumn get description => text()();
-}
-
-class Config extends Table {
-  TextColumn get phone => text()();
-  TextColumn get address => text()(); // Fixed a typo here
-  ColumnBuilder<double> get coordinates => real(); // Changed to RealColumn
-}
-
-class Order extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text()();
-  TextColumn get phone => text()();
-  TextColumn get type => text()();
-  TextColumn get hours => text()();
-}
-
-class Vacancy extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text()();
-  IntColumn get price => integer()();
-  TextColumn get description => text()();
-}
-
 @DriftDatabase(tables: [RentVehicle, Work, Config, Vacancy, Order])
 class Spectrdatabase extends _$Spectrdatabase {
-  Spectrdatabase(QueryExecutor e) : super(e);
+  Spectrdatabase() : super(_openConnection());
 
   int get schemaVersion => 1;
-
-  LazyDatabase _openConnection() {
-    // the LazyDatabase util lets us find the right location for the file async.
-    return LazyDatabase(() async {
-      // put the database file, called db.sqlite here, into the documents folder
-      // for your app.
-      final dbFolder = await getApplicationDocumentsDirectory();
-      final file = File(p.join(dbFolder.path, 'db.sqlite'));
-
-      return NativeDatabase.createInBackground(file);
-    });
   }
 
-/*  static Future<QueryExecutor> _openConnection() async {
-    final appDirectory = await getApplicationDocumentsDirectory();
-    final databasePath = join(appDirectory.path, 'spectrdatabase.db');
-    final executor = ;
-    return executor;
-  }*/
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'spectrdatabase.sqlite'));
+    final cachebase = (await getTemporaryDirectory()).path;
+    sqlite3.tempDirectory = cachebase;
+    return NativeDatabase.createInBackground(file);
+  });
 }
