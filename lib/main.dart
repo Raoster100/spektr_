c7e75652eb162dd2213a80161d9527ae283ecdcb
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-04-08 15:59:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:spektr/app/app.dart';
 
 
 void main() async {
-  await Hive.initFlutter();
- /* HiveService().init();*/
   runApp(const App());
 }
