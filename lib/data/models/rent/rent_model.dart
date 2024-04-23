98acb43088cdc9ef00b030d0522591ab4b5b8b2d
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-04-23 12:34:31 +0300

    Create rent_model.dart

---
+++ lib/data/models/rent/rent_model.dart
@@ -0,0 +1,20 @@
+import 'dart:typed_data';
+
+import 'package:dart_mappable/dart_mappable.dart';
+part 'rent_model.mapper.dart';
+
+@MappableClass()
+class RentModel with RentModelMappable {
+  RentModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.image
+  });
+  final int id;
+  final String name;
+  final int price;
+  final String description;
+  final Uint8List image;
+}
