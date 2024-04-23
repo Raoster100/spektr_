f54111c9d1e75c38f2080e895110273f9ad0f10e
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-04-23 12:34:20 +0300

    Create config_model.dart

---
+++ lib/data/models/config/config_model.dart
@@ -0,0 +1,20 @@
+import 'dart:typed_data';
+
+import 'package:dart_mappable/dart_mappable.dart';
+part 'config_model.mapper.dart';
+
+@MappableClass()
+class ConfigModel with ConfigModelMappable {
+  ConfigModel({
+    required this.id,
+    required this.address,
+    required this.phone,
+    required this.longitude,
+    required this.latitude
+  });
+  final int id;
+  final String address;
+  final String phone;
+  final double longitude;
+  final double latitude;
+}
