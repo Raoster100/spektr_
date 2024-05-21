9ad93866c0ce6f75983139dff2fdb5836fb8a285
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-05-21 11:03:58 +0300

    Update remote_work_ds.dart

--- lib/data/data_sources/work/remote_work_ds.dart
+++ lib/data/data_sources/work/remote_work_ds.dart
@@ -1,35 +1,28 @@
 import 'package:chopper/chopper.dart';
 import 'package:spektr/data/data_sources/core/app_remote_ds.dart';
 import 'package:spektr/data/models/models.dart';
+import 'package:spektr/data/models/work/work_dto.dart';
 
-/*
-part 'remote_work_ds.chopper.dart';*/
+/*part 'remote_work_ds.chopper.dart';*/
 
 @ChopperApi(baseUrl: 'Work')
 abstract class RemoteWorkDataSource extends ChopperService
-implements AppRemoteDataSource {/*
-  static RemoteWorkDataSource create([ChopperClient? client]) =>
+    implements AppRemoteDataSource {
+/*  static RemoteWorkDataSource create([ChopperClient? client]) =>
       _$RemoteWorkDataSource(client);*/
 
   @Get()
   Future<Response<List<WorkModel>>> fetch();
 
   @Get(path: r'/{id}')
-  Future<Response<List<WorkModel>>> getByWorkId(
-      @Path() final String id
-      );
+  Future<Response<List<WorkModel>>> getByWorkId(@Path() final String id);
 
-/*  @Post()
-  Future<Response<dynamic>> add(@Body() final)*/
+  @Post()
+  Future<Response<dynamic>> add(@Body() final WorkDto dto);
 
+  @Patch()
+  Future<Response<dynamic>> edit(@Body() final WorkDto dto);
 
-
-
-
-
-
-
-
-
-
+  @Delete()
+  Future<Response<dynamic>> delete(@Path() final String id);
 }
