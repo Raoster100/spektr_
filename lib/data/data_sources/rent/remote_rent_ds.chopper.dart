7c72c9e8c6b68d6cd0ad71601faf2323c9d0a5f0
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-05-21 17:05:03 +0300

    rent

---
+++ lib/data/data_sources/rent/remote_rent_ds.chopper.dart
@@ -0,0 +1,78 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'remote_rent_ds.dart';
+
+// **************************************************************************
+// ChopperGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _$RemoteRentDataSource extends RemoteRentDataSource {
+  _$RemoteRentDataSource([ChopperClient? client]) {
+    if (client == null) return;
+    this.client = client;
+  }
+
+  @override
+  final Type definitionType = RemoteRentDataSource;
+
+  @override
+  Future<Response<List<RentModel>>> fetch() {
+    final Uri $url = Uri.parse('Rent');
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+    );
+    return client.send<List<RentModel>, RentModel>($request);
+  }
+
+  @override
+  Future<Response<List<RentModel>>> getByRentId(String id) {
+    final Uri $url = Uri.parse('Rent/${id}');
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+    );
+    return client.send<List<RentModel>, RentModel>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> add(RentDto dto) {
+    final Uri $url = Uri.parse('Rent');
+    final $body = dto;
+    final Request $request = Request(
+      'POST',
+      $url,
+      client.baseUrl,
+      body: $body,
+    );
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> edit(RentDto dto) {
+    final Uri $url = Uri.parse('Rent');
+    final $body = dto;
+    final Request $request = Request(
+      'PATCH',
+      $url,
+      client.baseUrl,
+      body: $body,
+    );
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> delete(String id) {
+    final Uri $url = Uri.parse('Rent');
+    final Request $request = Request(
+      'DELETE',
+      $url,
+      client.baseUrl,
+    );
+    return client.send<dynamic, dynamic>($request);
+  }
+}
--- lib/data/data_sources/rent/remote_rent_ds.dart
+++ lib/data/data_sources/rent/remote_rent_ds.dart
@@ -3,13 +3,13 @@ import 'package:spektr/data/data_sources/core/app_remote_ds.dart';
 import 'package:spektr/data/models/models.dart';
 import '../../models/rent/rent_dto.dart';
 
-/*part 'remote_rent_ds.chopper.dart';*/
+part 'remote_rent_ds.chopper.dart';
 
 @ChopperApi(baseUrl: 'Rent')
 abstract class RemoteRentDataSource extends ChopperService
     implements AppRemoteDataSource {
-/*  static RemoteRentDataSource create([ChopperClient? client]) =>
-      _$RemoteRentDataSource(client);*/
+  static RemoteRentDataSource create([ChopperClient? client]) =>
+      _$RemoteRentDataSource(client);
 
   @Get()
   Future<Response<List<RentModel>>> fetch();
