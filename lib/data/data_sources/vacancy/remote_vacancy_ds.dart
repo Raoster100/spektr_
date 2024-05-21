892abba481e8922064b2ce736c6f01d00dfb98ac
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-05-21 11:03:56 +0300

    Update remote_vacancy_ds.dart

---
+++ lib/data/data_sources/vacancy/remote_vacancy_ds.dart
@@ -0,0 +1,28 @@
+import 'package:chopper/chopper.dart';
+import 'package:spektr/data/data_sources/core/app_remote_ds.dart';
+import 'package:spektr/data/models/models.dart';
+import '../../models/vacancy/vacancy_dto.dart';
+
+/*part 'remote_vacancy_ds.chopper.dart';*/
+
+@ChopperApi(baseUrl: 'Vacancy')
+abstract class RemoteVacancyDataSource extends ChopperService
+    implements AppRemoteDataSource {
+/*  static RemoteVacancyDataSource create([ChopperClient? client]) =>
+      _$RemoteVacancyDataSource(client);*/
+
+  @Get()
+  Future<Response<List<VacancyModel>>> fetch();
+
+  @Get(path: r'/{id}')
+  Future<Response<List<VacancyModel>>> getByVacancyId(@Path() final String id);
+
+  @Post()
+  Future<Response<dynamic>> add(@Body() final VacancyDto dto);
+
+  @Patch()
+  Future<Response<dynamic>> edit(@Body() final VacancyDto dto);
+
+  @Delete()
+  Future<Response<dynamic>> delete(@Path() final String id);
+}
