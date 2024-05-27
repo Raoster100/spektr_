39e0f65ec5ce078a6407a243cad8cad2aeb149ad
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-05-27 14:46:02 +0300

    Update app_service.dart

--- lib/domain/services/core/app_service.dart
+++ lib/domain/services/core/app_service.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/cupertino.dart';
-import '../../../data/data_sources/core/app_local_ds.dart';
 import '../../../data/data_sources/core/app_remote_ds.dart';
 
-abstract class AppService<Local extends AppLocalDataSource,
-Remote extends AppRemoteDataSource> {
-  AppService(this.lds, this.rds, );
+abstract class AppService<Remote extends AppRemoteDataSource> {
+  AppService(this.rds);
 
-  final Local lds;
   final Remote rds;
 
 
