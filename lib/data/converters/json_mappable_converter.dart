6f597e0e1626c193d11f19d130188cb5da6e55a3
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-05-27 14:45:33 +0300

    122

--- lib/data/converters/json_mappable_converter.dart
+++ lib/data/converters/json_mappable_converter.dart
@@ -1,31 +1,21 @@
-/*
+import 'dart:async';
+import 'dart:convert';
 import 'package:chopper/chopper.dart';
 import 'package:dart_mappable/dart_mappable.dart';
-import 'package:spektr/data/models/models.dart';
 
 class JsonMappableConverter extends JsonConverter {
   @override
-  Future<Response<ResultType>> convertResponse<ResultType, Item>(
+  FutureOr<Response> convertError<BodyType, InnerType>(
       Response response) async {
-    final jsonRes = await super.convertResponse(response);
-    return jsonRes.copyWith<ResultType>(
-      body: jsonRes.body == null || jsonRes.body is String
-          ? null
-          : Mapper.fromValue(jsonRes.body),
-    );
+    final json = jsonDecode(utf8.decode(response.bodyBytes));
+    return response.copyWith(body: body);
   }
 
   @override
-  // all objects should implements toJson method
-  Request convertRequest(Request request) {
-    if (request.body is String) {
-      return request;
-    }
-    return super.convertRequest(
-      request.copyWith(
-        body: Mapper.toMap(request.body),
-      ),
-    );
+  FutureOr<Response<BodyType>> convertResponse<BodyType, InnerType>(
+      Response response) async {
+    final json = jsonDecode(utf8.decode(response.bodyBytes));
+    final BodyType body = MapperContainer.globals.fromValue(json);
+    return response.copyWith(body: body);
   }
 }
-*/
