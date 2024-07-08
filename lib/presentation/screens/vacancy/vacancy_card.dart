03fe56102b22920c1f2083d14f02160209793bc5
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-08 10:03:32 +0300

    Update vacancy_card.dart

--- lib/presentation/screens/vacancy/vacancy_card.dart
+++ lib/presentation/screens/vacancy/vacancy_card.dart
@@ -44,6 +44,7 @@ class VacancyCardWidget extends StatelessWidget {
               child: Text(
                 lenght ?? 'Описание вакансии в котором будет что то написано',
                 style: AppTypography.inter.black.s16.w600,
+
               ),
             ),
             /*SizedBox(height: 22),
