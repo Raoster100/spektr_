714bf597bae934dcd5b93a1a7d992c6f1fa95484
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-15 13:06:26 +0300

    Update remote_order_ds.chopper.dart

--- lib/data/data_sources/order/remote_order_ds.chopper.dart
+++ lib/data/data_sources/order/remote_order_ds.chopper.dart
@@ -66,7 +66,7 @@ final class _$RemoteOrderDataSource extends RemoteOrderDataSource {
   }
 
   @override
-  Future<Response<dynamic>> delete(String id) {
+  Future<Response<dynamic>> delete(int id) {
     final Uri $url = Uri.parse('Order');
     final Request $request = Request(
       'DELETE',
