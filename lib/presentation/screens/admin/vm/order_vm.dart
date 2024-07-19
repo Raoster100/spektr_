bbe3515cbd07e6c910d63c3c7412ef165c787583
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-19 14:13:32 +0300

    Update order_vm.dart

--- lib/presentation/screens/admin/vm/order_vm.dart
+++ lib/presentation/screens/admin/vm/order_vm.dart
@@ -1,15 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:spektr/domain/services/order_service.dart';
 import 'package:stacked/stacked.dart';
+import '../../../../data/models/order/order_model.dart';
 
 class OrderViewModel extends BaseViewModel {
-  OrderViewModel(this.orderService, {
-    required this.id,
-    required this.name,
-    required this.phone,
-});
+  OrderViewModel(this.orderService);
 
   final OrderService orderService;
-  final String id;
-  final String name;
-  final String phone;
+  List<OrderModel> orders = [];
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+  final typeController = TextEditingController();
+  final hoursController = TextEditingController();
+  List<String> items = ['Аренда', 'Услуга'];
+  String? selectedItem = 'Аренда';
+
+  Future<void> fetch() async {
+    orders = await orderService.fetch();
+    notifyListeners();
+  }
+
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    phoneController.dispose();
+    typeController.dispose();
+    hoursController.dispose();
+  }
 }
