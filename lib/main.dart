0fed3529300364bd3d11e2ae29e62d89574804cc
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-22 12:10:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ import 'package:spektr/data/data_sources/core/app_remote_ds.dart';
 import 'package:spektr/data/data_sources/order/remote_order_ds.dart';
 import 'package:spektr/domain/services/core/app_service.dart';
 import 'package:spektr/domain/services/order_service.dart';
+import 'package:get_it/get_it.dart';
+
+
 
 
 void main() async {
   runApp(const App());
-
 }
