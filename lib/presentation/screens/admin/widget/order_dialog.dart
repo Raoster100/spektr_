6a25addb4e42ea576e80de0efa41dc7beb310ec5
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-22 12:17:09 +0300

    Update order_dialog.dart

--- lib/presentation/screens/admin/widget/order_dialog.dart
+++ lib/presentation/screens/admin/widget/order_dialog.dart
@@ -1,14 +1,16 @@
 // import 'package:flutter/cupertino.dart';
 // import 'package:flutter/material.dart';
 // import 'package:flutter/widgets.dart';
+// import 'package:spektr/domain/services/order_service.dart';
 // import 'package:spektr/presentation/screens/admin/vm/order_vm.dart';
 // import 'package:stacked/stacked.dart';
 //
 // class OrderDialog extends StackedView<OrderViewModel> {
-//   const OrderDialog({
+//   const OrderDialog(this.orderService, {
 //     super.key,
 //   });
 //
+// final OrderService orderService;
 //
 //   @override
 //   Widget builder(BuildContext context, OrderViewModel viewModel, Widget? child) {
