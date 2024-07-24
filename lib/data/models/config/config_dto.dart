a84b6739049d1b756db570dc14e3cece04017bd7
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-24 16:15:02 +0300

    Update config_dto.dart

--- lib/data/models/config/config_dto.dart
+++ lib/data/models/config/config_dto.dart
@@ -4,13 +4,13 @@ part 'config_dto.mapper.dart';
 @MappableClass()
 class ConfigDto with ConfigDtoMappable {
   ConfigDto({
-    required this.id,
+    this.id,
     this.address,
     this.phone,
     this.longitude,
     this.latitude
   });
-  final int id;
+  final int? id;
   final String? address;
   final String? phone;
   final double? longitude;
