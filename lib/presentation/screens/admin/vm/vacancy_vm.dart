5804990136cfee49038e06bb2c6305e317c08fbc
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-25 00:38:30 +0300

    Update vacancy_vm.dart

--- lib/presentation/screens/admin/vm/vacancy_vm.dart
+++ lib/presentation/screens/admin/vm/vacancy_vm.dart
@@ -1,13 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:spektr/data/models/models.dart';
+import 'package:spektr/domain/services/vacancy_service.dart';
 import 'package:stacked/stacked.dart';
 
-class VacancyViewModel extends BaseViewModel {
-  VacancyViewModel( {
-    required this.id,
-    required this.name,
-    required this.phone,
-  });
+import '../../../../data/models/vacancy/vacancy_dto.dart';
 
-  final String id;
-  final String name;
-  final String phone;
+class VacancyViewModel extends BaseViewModel {
+  VacancyViewModel(this.vacancyService);
+
+  final nameController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final priceController = TextEditingController();
+  final VacancyService vacancyService;
+  List<VacancyModel> vacancy = [];
+
+  Future<void> addVacancy() async {
+    await vacancyService.add(VacancyDto(
+        name: nameController.text,
+        price: int.tryParse(priceController.text) ?? 0));
+  }
+
+  Future<void> fetchOrder() async {
+    vacancy = await vacancyService.fetch();
+    notifyListeners();
+  }
+
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    descriptionController.dispose();
+    priceController.dispose();
+  }
 }
