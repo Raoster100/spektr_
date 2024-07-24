0914978ceb6589c0a5944c589c948afea2941292
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-24 16:15:06 +0300

    Update order_dto.dart

--- lib/data/models/order/order_dto.dart
+++ lib/data/models/order/order_dto.dart
@@ -4,13 +4,13 @@ part 'order_dto.mapper.dart';
 @MappableClass()
 class OrderDto with OrderDtoMappable {
   OrderDto({
-    required this.id,
+    this.id,
     required this.name,
     required this.phone,
     this.type,
     this.hours
   });
-  final int id;
+  final int? id;
   final String name;
   final String phone;
   final String? type;
