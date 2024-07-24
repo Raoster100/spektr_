f16044dc488e70b57ff770be58da99000380d872
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-24 16:15:22 +0300

    Update rent_vm.dart

--- lib/presentation/screens/admin/vm/rent_vm.dart
+++ lib/presentation/screens/admin/vm/rent_vm.dart
@@ -1,13 +1,33 @@
+import 'dart:typed_data';
+import 'package:flutter/cupertino.dart';
+import 'package:spektr/domain/services/rent_service.dart';
 import 'package:stacked/stacked.dart';
+import '../../../../data/models/rent/rent_dto.dart';
+
 
 class RentViewModel extends BaseViewModel {
-  RentViewModel( {
-    required this.id,
-    required this.name,
-    required this.phone,
-  });
+  RentViewModel(this.rentService);
 
-  final String id;
-  final String name;
-  final String phone;
+  final nameController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final priceController = TextEditingController();
+  Uint8List? pickedImage;
+  final RentService rentService;
+
+
+
+  Future<void> fetch() async {
+    await rentService.fetch();
+    notifyListeners();
+  }
+
+  Future<void> addRent() async {
+    await rentService.add(RentDto(name: nameController.text, price: int.tryParse(priceController.text) ?? 0));
+  }
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    descriptionController.dispose();
+    priceController.dispose();
+  }
 }
