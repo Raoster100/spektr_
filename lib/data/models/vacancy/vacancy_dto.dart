7cb3f117a623099302128cc6c5ce5ec5b9f20362
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-24 13:19:29 +0300

    Update vacancy_dto.dart

--- lib/data/models/vacancy/vacancy_dto.dart
+++ lib/data/models/vacancy/vacancy_dto.dart
@@ -4,12 +4,12 @@ part 'vacancy_dto.mapper.dart';
 @MappableClass()
 class VacancyDto with VacancyDtoMappable {
   VacancyDto({
-    required this.id,
+    this.id,
     required this.name,
     required this.price,
     this.description
   });
-  final int id;
+  final int? id;
   final String name;
   final int price;
   final String? description;
