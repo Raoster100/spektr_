db69b98389de9cd5d2f3b57049179f1c7abce7b6
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-25 00:38:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
-import 'dart:io';
-import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
 import 'package:spektr/app/app.dart';
-import 'package:spektr/data/data_sources/config/remote_config_ds.dart';
-import 'package:spektr/data/data_sources/core/app_remote_ds.dart';
-import 'package:spektr/data/data_sources/order/remote_order_ds.dart';
-import 'package:spektr/domain/services/core/app_service.dart';
-import 'package:spektr/domain/services/order_service.dart';
-import 'package:get_it/get_it.dart';
+
 
 
 
