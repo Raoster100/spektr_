26423fd65ad0becc84b302f20b6012c386b7ae42
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-25 00:38:37 +0300

    Update vacancy_dialog.dart

--- lib/presentation/screens/admin/widget/vacancy_dialog.dart
+++ lib/presentation/screens/admin/widget/vacancy_dialog.dart
@@ -1,33 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:spektr/data/models/vacancy/vacancy_dto.dart';
 import 'package:spektr/domain/services/vacancy_service.dart';
+import 'package:spektr/presentation/screens/admin/vm/vacancy_vm.dart';
+import 'package:stacked/stacked.dart';
 
 late final VacancyService vacancyService;
 
-class VacancyDialog extends StatefulWidget {
+class VacancyDialog extends StackedView<VacancyViewModel> {
   const VacancyDialog({
     super.key,
   });
 
   @override
-  State<VacancyDialog> createState() => _VacancyDialogState();
-}
-
-class _VacancyDialogState extends State<VacancyDialog> {
-  final nameController = TextEditingController();
-  final descriptionController = TextEditingController();
-  final priceController = TextEditingController();
-
-  Future<void> addVacancy() async {
-    await vacancyService.add(VacancyDto(
-        name: nameController.text,
-        price: int.tryParse(priceController.text) ?? 0));
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget builder(
+      BuildContext context, VacancyViewModel viewModel, Widget? child) {
     return Dialog(
       child: SizedBox(
         width: 900,
@@ -42,25 +29,26 @@ class _VacancyDialogState extends State<VacancyDialog> {
                     width: 300,
                     child: TextFormField(
                       decoration: InputDecoration(hintText: 'Название'),
-                      controller: nameController,
+                      controller: viewModel.nameController,
                     ),
                   ),
                   SizedBox(
                     width: 300,
                     child: TextFormField(
                       decoration: InputDecoration(hintText: 'Описание'),
-                      controller: descriptionController,
+                      controller: viewModel.descriptionController,
                     ),
                   ),
                   SizedBox(
                     width: 300,
                     child: TextFormField(
                       decoration: InputDecoration(hintText: 'Цена'),
-                      controller: priceController,
+                      controller: viewModel.priceController,
                     ),
                   ),
                   CupertinoButton(
-                      child: Text('Добавить вакансию'), onPressed: addVacancy),
+                      child: Text('Добавить вакансию'),
+                      onPressed: viewModel.addVacancy),
                   CupertinoButton(
                       child: Text('Отменить'),
                       onPressed: Navigator.of(context).pop)
@@ -71,5 +59,10 @@ class _VacancyDialogState extends State<VacancyDialog> {
         ),
       ),
     );
+  }
+
+  @override
+  VacancyViewModel viewModelBuilder(BuildContext context) {
+    return VacancyViewModel(vacancyService);
   }
 }
