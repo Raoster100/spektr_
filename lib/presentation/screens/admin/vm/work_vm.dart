aacb21c76fed9d0cd686673bb88c0a78fbb4662a
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-07-25 00:38:32 +0300

    Update work_vm.dart

--- lib/presentation/screens/admin/vm/work_vm.dart
+++ lib/presentation/screens/admin/vm/work_vm.dart
@@ -1,13 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:spektr/domain/services/work_service.dart';
 import 'package:stacked/stacked.dart';
 
-class WorkViewModel extends BaseViewModel {
-  WorkViewModel( {
-    required this.id,
-    required this.name,
-    required this.phone,
-  });
+import '../../../../data/models/work/work_dto.dart';
 
-  final String id;
-  final String name;
-  final String phone;
+class WorkViewModel extends BaseViewModel {
+  WorkViewModel(this.workService);
+
+  final nameController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final priceController = TextEditingController();
+  final WorkService workService;
+
+  Future<void> addWork() async {
+    await workService.add(WorkDto(
+        name: nameController.text,
+        price: int.tryParse(priceController.text) ?? 0));
+  }
+
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    descriptionController.dispose();
+    priceController.dispose();
+  }
 }
