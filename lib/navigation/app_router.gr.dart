16fe12167d3db4f551df955f8db55270c83f16c4
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-08-02 12:17:29 +0300

    Update app_router.gr.dart

--- lib/navigation/app_router.gr.dart
+++ lib/navigation/app_router.gr.dart
@@ -21,6 +21,20 @@ abstract class _$AppRouter extends RootStackRouter {
         child: const AboutView(),
       );
     },
+    Admin.name: (routeData) {
+      final args = routeData.argsAs<AdminArgs>();
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: AdminView(
+          args.rentService,
+          args.orderService,
+          args.workService,
+          args.vacancyService,
+          args.configService,
+          key: args.key,
+        ),
+      );
+    },
     Login.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
@@ -66,6 +80,63 @@ class About extends PageRouteInfo<void> {
   static const String name = 'About';
 
   static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [AdminView]
+class Admin extends PageRouteInfo<AdminArgs> {
+  Admin({
+    required RentService rentService,
+    required OrderService orderService,
+    required WorkService workService,
+    required VacancyService vacancyService,
+    required ConfigService configService,
+    Key? key,
+    List<PageRouteInfo>? children,
+  }) : super(
+          Admin.name,
+          args: AdminArgs(
+            rentService: rentService,
+            orderService: orderService,
+            workService: workService,
+            vacancyService: vacancyService,
+            configService: configService,
+            key: key,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'Admin';
+
+  static const PageInfo<AdminArgs> page = PageInfo<AdminArgs>(name);
+}
+
+class AdminArgs {
+  const AdminArgs({
+    required this.rentService,
+    required this.orderService,
+    required this.workService,
+    required this.vacancyService,
+    required this.configService,
+    this.key,
+  });
+
+  final RentService rentService;
+
+  final OrderService orderService;
+
+  final WorkService workService;
+
+  final VacancyService vacancyService;
+
+  final ConfigService configService;
+
+  final Key? key;
+
+  @override
+  String toString() {
+    return 'AdminArgs{rentService: $rentService, orderService: $orderService, workService: $workService, vacancyService: $vacancyService, configService: $configService, key: $key}';
+  }
 }
 
 /// generated route for
