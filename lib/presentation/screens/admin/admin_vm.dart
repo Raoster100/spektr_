9b12e38cded764d2fae3b18ab0dcfd06e70cab8c
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-08-02 12:17:40 +0300

    Update admin_vm.dart

--- lib/presentation/screens/admin/admin_vm.dart
+++ lib/presentation/screens/admin/admin_vm.dart
@@ -1,3 +1,4 @@
+import 'package:spektr/domain/services/config_service.dart';
 import 'package:spektr/domain/services/order_service.dart';
 import 'package:spektr/domain/services/rent_service.dart';
 import 'package:spektr/domain/services/vacancy_service.dart';
@@ -5,10 +6,11 @@ import 'package:spektr/domain/services/work_service.dart';
 import 'package:stacked/stacked.dart';
 
 class AdminViewModel extends BaseViewModel {
-  AdminViewModel(this.rentService, this.orderService, this.workService, this.vacancyService);
+  AdminViewModel(this.rentService, this.orderService, this.workService, this.vacancyService, this.configService);
   final RentService rentService;
   final OrderService orderService;
   final WorkService workService;
   final VacancyService vacancyService;
+  final ConfigService configService;
 }
 
