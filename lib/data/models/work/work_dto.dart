fa3082f27fb1c72521cf9df8ca5ca6fb2feb61b8
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-08-12 11:04:05 +0300

    Update work_dto.dart

--- lib/data/models/work/work_dto.dart
+++ lib/data/models/work/work_dto.dart
@@ -1,13 +1,16 @@
 import 'package:dart_mappable/dart_mappable.dart';
+
 part 'work_dto.mapper.dart';
+
 @MappableClass()
 class WorkDto with WorkDtoMappable {
   WorkDto({
     this.id,
     required this.name,
     required this.price,
-    this.description
+    this.description,
   });
+
   final int? id;
   final String name;
   final int price;
