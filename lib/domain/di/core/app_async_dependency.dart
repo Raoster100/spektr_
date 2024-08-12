23da750b9c0b4515a168b144f0371b301bc079ad
ReworkID <75668811+Raoster100@users.noreply.github.com>
2024-08-12 11:04:09 +0300

    Update app_async_dependency.dart

--- lib/domain/di/core/app_async_dependency.dart
+++ lib/domain/di/core/app_async_dependency.dart
@@ -5,6 +5,7 @@ abstract class AppAsyncDependency {
   AppAsyncDependency(BuildContext context) {
     init(context).then((value) => inited.complete());
   }
+
   final Completer inited = Completer();
 
   Future<void> init(BuildContext context);
